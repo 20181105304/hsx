2d9d528e5b10c3fd882696147e5d696b58c755fa
20181105304 <1924865152@qq.com>
2019-06-13 12:04:58 +0800

    Initial Commit

---
+++ hsx/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  hsx
+//
+//  Created by s20181105304 on 2019/6/13.
+//  Copyright © 2019 s20181105304. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
